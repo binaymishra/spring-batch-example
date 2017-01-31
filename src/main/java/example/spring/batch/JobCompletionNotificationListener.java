cc4ce2160f6d46b9f46e1fbca47d42ef5848d925
Binay Mishra <binaymishra@gmail.com>
2017-01-31 13:38:13 +0530

    Batch Job Completion listener for checking result, may not be necessary in actual senario.

---
+++ src/main/java/example/spring/batch/JobCompletionNotificationListener.java
@@ -0,0 +1,44 @@
+package example.spring.batch;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+			System.err.println("!!! JOB FINISHED !!!");
+
+			List<PurchaseOrder> results = jdbcTemplate.query("SELECT ID, ORDER_NAME FROM PURCHASE_ORDER", new RowMapper<PurchaseOrder>() {
+				@Override
+				public PurchaseOrder mapRow(ResultSet rs, int row) throws SQLException {
+					return new PurchaseOrder(rs.getInt(1), rs.getString(2));
+				}
+			});
+
+			for (PurchaseOrder purchaseOrder : results) {
+				System.err.println(purchaseOrder + " inside DB.");
+			}
+
+		}
+	}
+}
